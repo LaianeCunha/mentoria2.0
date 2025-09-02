df8289ef7b74bc2e19b3600ad5161b257bf1f7d5
Laiane Cunha <laianecunha20@gmail.com>
2025-09-02 11:28:05 -0300

    Função renomeada e adicionado a opção vaca no retorno

--- linguagem.js
+++ linguagem.js
@@ -3,6 +3,8 @@ function linguagemDosGatosECachorros(animal) {
         return 'au au' 
     else if (animal == 'gato') 
         return 'miau' 
+    else if (animal == 'vaca')
+        return 'muuuu'
     else
         return 'Informe gato ou cachorro'
  }
